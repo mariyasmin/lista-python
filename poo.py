e6c5c4987718ff1fd97bb6c4c739a4fda10edd7d
mariyasmin <mari.yasminnnn@gmail.com>
2022-04-29 07:38:39 -0300

    add-lista-poo

---
+++ poo.py
@@ -0,0 +1,15 @@
+class Usuario:
+    nome = str(input("Digite seu primeiro nome: "))
+    sobrenome = str(input("Digite seu sobrenome: "))    
+
+    segundonome = "Jane"
+    segundosobrenome = "Silva"
+
+    def hello(self):
+        return "Olá"
+
+usuario1= Usuario()
+usuario2 = Usuario()
+print(usuario1.hello(), usuario1.nome , usuario1.sobrenome) 
+print(usuario2.hello(), usuario2.segundonome , usuario2.segundosobrenome)
+
